2341a66813b8d626249fd16adcb12a472eded1a6
Renato Ferreira <renatoferreira249@gmail.com>
2017-10-03 06:37:02 -0300

    n deu tempo de testar nem terminar

--- cidadeAleatoria.py
+++ cidadeAleatoria.py
@@ -1,20 +1,45 @@
-def calculaCriminalidade(atomo, centroCartesiano, atomosCrimCalculada):
+import math
+from random import randint
+
+def distanciaDoisPontos(ponto1, ponto2):
+    return math.sqrt(pow(ponto1.x - ponto2.x) + pow(ponto1.y - ponto2.y))
+
+def calculaCriminalidade(atomo, atomosCrimCalculada):
     vizCrimCalculada = [ x for x in atomo.vizinhos and x in atomosCrimCalculada ]
     if len(vizCrimCalculada > 1):
-        #atomo recebe a média arredondada dos crimes dos vizinhos
-        atomo = 
+        acmCrimesViz = 0
+        for vizinho in vizCrimCalculada:
+            acmCrimesViz += vizinho.qtdCrimes
+        return acmCrimesViz/len(vizCrimCalculada)
+
     else:
-        """
-        distanciaDoisPontos (classe utilMat) - maior distancia plano cartesiano
-        * número aleatório 1-3
-        """
-        atomo.qtdCrimes = 
+        return ( 1/sqrt(pow(atomo.posicao.x) + pow(atomo.posicao.y)) 
+                * randint(100, 300))
 
-def geraCidadeAleatoria(tamanhoXY, centroCartesiano, calculaCriminalidade):
-    cidade = [[None]*numAtomosY]*numAtomosX
+def achaVizinhos(atomo, atomos, tamanhoXY):
+    vizinhos = []
+    for x in range(atomo.posicao.x - 1, atomo.posicao.x + 2):
+        for y in range(atomo.posicao.y - 1, atomo.posicao.y + 2):
+            if ( x == y ):
+                continue
+            elif (0 <= x < tamanhoXY.x) and (0 <= y < tamanhoXY.y):
+                vizinhos.append(atomos[tamanhoXY.x * x + y])
+
+    return vizinhos        
     
 
-
+def geraCidadeAleatoria(tamanhoXY, calculaCriminalidade):
+    Atomo = namedtuple('Atomo', ['nome', 'posicao', 'qtdCrimes', 'vizinhos'])
+    atomos = [ Atomo(posicao = (x,y)) for x in range(tamanhoXY.x) 
+            for y in range(tamanhoXY.sty) ]
     
+    atomosVizinhosCalculados = [ Atomo(posicao = atomo.posicao,
+            vizinhos = achaVizinhos(atomo, atomos, tamanhoXY)) 
+            for atomo in atomos ]
 
-
+    atomosCrimCalculada = []
+    for atomo in  atomosVizinhosCalculados:
+        atomo.qtdCrimes = calculaCriminalidade(atomo, atomosCrimCalculada)
+        atomosCrimCalculada.append(atomo)
+    
+    return atomosCrimCalculada
--- distritoGuloso.py
+++ distritoGuloso.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+"""
 def criaDistritos(atomos, seeds, numIters):
 
     def retornaUmVizinho(cluster):
@@ -21,5 +23,71 @@ def criaDistritos(atomos, seeds, numIters):
             for i in range(len(seeds)):
                 novoAtomo = retornaUmVizinho(clusters[i])
                 vizinhos[i].append(novoAtomo.vizinhos)
+
+"""
+
+def somaViolenciaCluster(cluster):
+    violencia = 0
+    for atomo in cluster.elementos:
+        violencia += atomo.qtdCrimes()
+    return violencia
+"""
+def retornaClusterMaisViolento(clusters):
+    clusterMaisViolento = clusters[0]
+    qtdCrimesClusterMV = somaViolenciaCluster(clusters[0]) 
+
+    for cluster in clusters:
+        violenciaCluster =  somaViolenciaCluster(cluster)
+        if violenciaCluster > qtdCrimesClusterMV:
+            clusterMaisViolento = cluster 
+            qtdCrimesCluster = violenciaCluster
+
+    return clusterMaisViolento
+"""
+
+def retornaClusterMenosViolento(clusters):
+
+    clusterMenosViolento = clusters[0]
+    qtdCrimesClusterMV = somaViolenciaCluster(clusters[0]) 
+
+    for cluster in clusters:
+        violenciaCluster =  somaViolenciaCluster(cluster)
+        if violenciaCluster < qtdCrimesClusterMV:
+            clusterMaisViolento = cluster 
+            qtdCrimesClusterMV = violenciaCluster
+
+    return clusterMaisViolento
+
+def retornaNovosVizinhos(cluster, novoAtomoAdicionado):
+    vizinhos = [ vizinho for vizinho in 
+
+def clusterizacaoGulosa(seeds, desvioPadraoMin, maxIters):
+    """
+        treta aqui:
+        maxIters tem que ser maior que o número de atomos
+        e tem que fzr um procedimento extra ( ou mudar o atual )
+        para os atomosque sobrarem, eles podem sobrar caso um cluster fique
+        cercado, considerando esse algoritmo que vc planejava programar
+    """
+
+    Cluster = namedtuple('Cluster', ['elementos', 'vizinhos'])
+    clusters = [Cluster([seed],seed.vizinhos) for seed on seeds]
+    
+    for i in range(maxIters):
+        clusterMenosViolento = retornaClusterMenosViolento(clusters)
+        clusterMenosViolento.elementos.append(clusterMenosViolento.vizinhos[0])
+        clusterMenosViolento.vizinhos = 
+
+        
+"""
+def divClustersSameSize(atomos, numClusters):
+
+    def criaClusterVizinhos(atomoSeed, atomos, tamanhoCluster):
+        atomosNaoSelecionados = [ atomo for atomo in atomos 
+                and atomo not is atomoSeed ]
+        cluster = []
+        while len(cluster) < tamanhoCluster and len(atomosNaoSelecionados) > 0:
+
+ """       
 
 
