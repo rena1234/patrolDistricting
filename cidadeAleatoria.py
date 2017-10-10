1385ff101c25e5017665a68e8af6f693a112a213
Renato Ferreira <renatoferreira249@gmail.com>
2017-10-10 06:26:35 -0300

    rodando mas dando o msm resultado

--- cidadeAleatoria.py
+++ cidadeAleatoria.py
@@ -1,45 +1,70 @@
+"""
+    Warning
+        TA TD ERRADO NESSA merda
+        PQ CADA FUNÇÂO TÀ TRATANDO O CENTRO DO SISTEMA COMO ALGO DIFERENTE
+
+        
+"""
 import math
 from random import randint
+from collections import namedtuple
 
 def distanciaDoisPontos(ponto1, ponto2):
     return math.sqrt(pow(ponto1.x - ponto2.x) + pow(ponto1.y - ponto2.y))
-
-def calculaCriminalidade(atomo, atomosCrimCalculada):
-    vizCrimCalculada = [ x for x in atomo.vizinhos and x in atomosCrimCalculada ]
-    if len(vizCrimCalculada > 1):
-        acmCrimesViz = 0
-        for vizinho in vizCrimCalculada:
-            acmCrimesViz += vizinho.qtdCrimes
-        return acmCrimesViz/len(vizCrimCalculada)
-
-    else:
-        return ( 1/sqrt(pow(atomo.posicao.x) + pow(atomo.posicao.y)) 
-                * randint(100, 300))
 
 def achaVizinhos(atomo, atomos, tamanhoXY):
     vizinhos = []
     for x in range(atomo.posicao.x - 1, atomo.posicao.x + 2):
         for y in range(atomo.posicao.y - 1, atomo.posicao.y + 2):
-            if ( x == y ):
+            if ( x == atomo.posicao.x and y == atomo.posicao.y ):
                 continue
             elif (0 <= x < tamanhoXY.x) and (0 <= y < tamanhoXY.y):
                 vizinhos.append(atomos[tamanhoXY.x * x + y])
-
     return vizinhos        
-    
 
-def geraCidadeAleatoria(tamanhoXY, calculaCriminalidade):
+def calculaCriminalidade(posicaoNovoAtomo, criminalidadeVizinhos, tamanhoXY):
+    if len(criminalidadeVizinhos) > 1:
+        return int(sum(criminalidadeVizinhos)/len(criminalidadeVizinhos))
+
+    return int( 1/(abs(posicaoNovoAtomo.x - tamanhoXY.x) 
+            + abs(posicaoNovoAtomo.y - tamanhoXY. y) + 1) * randint(100, 300))
+
+def geraCidadeAleatoria(tamanhoXY):
+
+    def achaVizinhosJaCriados(tamanhoXY, posicaoAtomo, atomos):
+
+        if posicaoAtomo.x == 0 and posicaoAtomo.y == 0:
+            return []
+
+        if posicaoAtomo.y == 0:
+            indiceBase = ((posicaoAtomo.x - 1 ) * tamanhoXY.x + 
+                    posicaoAtomo.y)
+            return [ a for a in atomos[indiceBase: indiceBase + 1]]
+        
+        if x == 0:
+            return [ atomos[posicaoAtomo.x * tamanhoXY.x + posicaoAtomo.y -1]]            
+
+        indiceBase = (posicaoAtomo.x - 1 ) * tamanhoXY.x + posicaoAtomo.y - 1
+        vizinhosCima = [ a for a in atomos[indiceBase: indiceBase + 2]]
+        return vizinhosCima + [
+                atomos[posicaoAtomo.x * tamanhoXY.x + posicaoAtomo.y - 1] ]
+
     Atomo = namedtuple('Atomo', ['nome', 'posicao', 'qtdCrimes', 'vizinhos'])
-    atomos = [ Atomo(posicao = (x,y)) for x in range(tamanhoXY.x) 
-            for y in range(tamanhoXY.sty) ]
-    
-    atomosVizinhosCalculados = [ Atomo(posicao = atomo.posicao,
-            vizinhos = achaVizinhos(atomo, atomos, tamanhoXY)) 
-            for atomo in atomos ]
+    Ponto = namedtuple('Ponto', ['x','y'])
 
-    atomosCrimCalculada = []
-    for atomo in  atomosVizinhosCalculados:
-        atomo.qtdCrimes = calculaCriminalidade(atomo, atomosCrimCalculada)
-        atomosCrimCalculada.append(atomo)
+    atomos = []
+    for x in range(tamanhoXY.x):
+        for y in range(tamanhoXY.y):
+            posicao = Ponto(x,y)
+            qtdCrimes = calculaCriminalidade(posicao,[vizinho.qtdCrimes 
+                    for vizinho 
+                    in achaVizinhosJaCriados(tamanhoXY, posicao, atomos)],
+                    tamanhoXY)
+            atomos.append(Atomo('a', posicao, qtdCrimes, []))
+
+    cidadeAleatoria = [ Atomo(atomo.nome, atomo.posicao, atomo.qtdCrimes, 
+            achaVizinhos(atomo, atomos, tamanhoXY))
+            for atomo in atomos ]
     
-    return atomosCrimCalculada
+    
+    return cidadeAleatoria
--- distritoGuloso.py
+++ distritoGuloso.py
@@ -1,93 +1,169 @@
+
+"""
+    Warning
+        TA TD ERRADO NESSA merda
+        PQ CADA FUNÇÂO TÀ TRATANDO O CENTRO DO SISTEMA COMO ALGO DIFERENTE
+
+        A FUNÇÂO QUE RETORNA OS SEEDS ESTÀ CORRETA!!!!
+
+        DÀ pra manter se vc mudar a função que calcula a criminalidade, para aceitar
+        um centro cartesiano que n seja 0,0 e sim x/2,y/2
+
+        o mesmo para a função que determina os  seeds
+"""
+
+"""
+    CENTRO CARTESIANO AGR EH x/2,y/2
+
+"""
+
 from copy import deepcopy
-"""
-def criaDistritos(atomos, seeds, numIters):
+from random import randint
+import math
+from cidadeAleatoria import geraCidadeAleatoria
+from statistics import pstdev
+from collections import namedtuple
 
-    def retornaUmVizinho(cluster):
-        vizinho = None
-        for atomo in cluster:
-            #usar if ternario aqui
-            if atomo.vizinhos not #vazio:
-                vizinho  = atomo.vizinhos[0]
-                break
-        return vizinho
+def achaPontosCirculo(raio, numPontos):
+    """
+        Warning, essa merda gera numero negativo
 
-    def divClustersSameSize(seeds, atomos):
-        clusters = [[]]
+        Mas agr vc qrr ponto negativo, viva o coupling!
+    """
+    Ponto = namedtuple('Ponto', ['x','y'])
+    pontos = []
+    for i in range(numPontos):
+        x = int(raio * math.cos(2 * math.pi * i / numPontos))
+        y = int(raio * math.sin(2 * math.pi * i / numPontos))
+        ponto = Ponto(x,y)
+        pontos.append(ponto)
 
-        #list comprehension for 2d array
-        vizinhosClusters = [[] x for  ]
-        atomosNaoSelecionados = [ x for x in atomos if not x in seeds ]
-        
-        #enquanto atomosNaoSelecionados for diferente de vazio
-        while():
-            for i in range(len(seeds)):
-                novoAtomo = retornaUmVizinho(clusters[i])
-                vizinhos[i].append(novoAtomo.vizinhos)
+    return pontos
 
-"""
+def determinaPosicoesSeeds(
+        numSeeds, numCirculos, raioCirculoMenor, proporcaoCirculos, tamanhoXY):
+
+    pontosPorCirculo = int(numSeeds / numCirculos)
+    pontos = []
+    raio = raioCirculoMenor
+    
+    for i in range(numCirculos):
+        #pontos.append(achaPontosCirculo(raio, pontosPorCirculo))
+        pontos += achaPontosCirculo(raio, pontosPorCirculo)
+        raio *= proporcaoCirculos
+    
+    Ponto = namedtuple('Ponto', ['x','y'])
+    return [Ponto(int(ponto.x + tamanhoXY.x/2), int(ponto.y + tamanhoXY.y/2))
+            for ponto in pontos]
+
+def achaSeeds(atomos, posicoesSeeds):
+    """
+        Dá treta aqui pq são namedtuples no posicoesSeeds
+        essa merda pode ser inclusive um objeto...
+    """
+    seeds = [ a for a in atomos if 
+            any(p.x == a.posicao.x and p.y == a.posicao.y 
+            for p in posicoesSeeds)]
+    
+    return seeds
 
 def somaViolenciaCluster(cluster):
     violencia = 0
     for atomo in cluster.elementos:
-        violencia += atomo.qtdCrimes()
+        violencia += atomo.qtdCrimes
     return violencia
-"""
-def retornaClusterMaisViolento(clusters):
-    clusterMaisViolento = clusters[0]
-    qtdCrimesClusterMV = somaViolenciaCluster(clusters[0]) 
 
-    for cluster in clusters:
-        violenciaCluster =  somaViolenciaCluster(cluster)
-        if violenciaCluster > qtdCrimesClusterMV:
-            clusterMaisViolento = cluster 
-            qtdCrimesCluster = violenciaCluster
-
-    return clusterMaisViolento
-"""
-
-def retornaClusterMenosViolento(clusters):
-
+def retIndClusterComVizMenosViolento(clusters):
     clusterMenosViolento = clusters[0]
     qtdCrimesClusterMV = somaViolenciaCluster(clusters[0]) 
-
+    
+    i = 0; indiceMenosViolento = 0
     for cluster in clusters:
         violenciaCluster =  somaViolenciaCluster(cluster)
-        if violenciaCluster < qtdCrimesClusterMV:
-            clusterMaisViolento = cluster 
+        if violenciaCluster < qtdCrimesClusterMV and len(cluster.vizinhos) > 0:
+            clusterMenosViolento = cluster 
             qtdCrimesClusterMV = violenciaCluster
+            indiceMenosViolento = i
+        i+=1
+    return indiceMenosViolento
 
-    return clusterMaisViolento
+def retornaVizinhosAtualizado(cluster, novoAtomoAdicionado):
+    vizinhancaAnterior = [ vizinho for vizinho in cluster.vizinhos 
+            if not vizinho is novoAtomoAdicionado ]
+    vizinhosNovos = [ x for x in novoAtomoAdicionado.vizinhos
+            if x not in vizinhancaAnterior ]
+    return vizinhancaAnterior + vizinhosNovos 
 
-def retornaNovosVizinhos(cluster, novoAtomoAdicionado):
-    vizinhos = [ vizinho for vizinho in 
-
-def clusterizacaoGulosa(seeds, desvioPadraoMin, maxIters):
-    """
-        treta aqui:
-        maxIters tem que ser maior que o número de atomos
-        e tem que fzr um procedimento extra ( ou mudar o atual )
-        para os atomosque sobrarem, eles podem sobrar caso um cluster fique
-        cercado, considerando esse algoritmo que vc planejava programar
-    """
-
+def clusterizaGuloso(seeds):
     Cluster = namedtuple('Cluster', ['elementos', 'vizinhos'])
-    clusters = [Cluster([seed],seed.vizinhos) for seed on seeds]
-    
-    for i in range(maxIters):
-        clusterMenosViolento = retornaClusterMenosViolento(clusters)
-        clusterMenosViolento.elementos.append(clusterMenosViolento.vizinhos[0])
-        clusterMenosViolento.vizinhos = 
+    clusters = [Cluster([seed],seed.vizinhos) for seed in seeds]
 
+    clustersComVizinhos = [ x for x in clusters if len(x.vizinhos) > 0 ]
+    while len(clustersComVizinhos) > 0:
+        i = retIndClusterComVizMenosViolento(clusters)
+        novoAtomoAdicionado = clusters[i].vizinhos[0]
+        clusters[i].elementos.append(novoAtomoAdicionado)
+        clusters[i] = Cluster(clusters[i].elementos,
+                retornaVizinhosAtualizado(clusters[i], novoAtomoAdicionado)) 
+
+        # retira o novoAtomoAdicionado dos vizinhos dos outros clusters
+        clusters = [ Cluster(x.elementos, [ v for v in x.vizinhos 
+                if not (v is novoAtomoAdicionado) ]) for x in clusters  ]
         
+        clustersComVizinhos = [ x for x in clusters if len(x.vizinhos) > 0 ]
+    
+    return clusters
+
+
+def clusterizaAleatorio(seeds):
+    Cluster = namedtuple('Cluster', ['elementos', 'vizinhos'])
+    clusters = [Cluster([seed],seed.vizinhos) for seed in seeds]
+
+    clustersComVizinhos = [ x for x in clusters if len(x.vizinhos) > 0 ]
+    while len(clustersComVizinhos) > 0:
+        i = randint(0, len(seeds) - 1)
+        while not len(clusters[i].vizinhos) > 0 :
+            i = randint(0, len(seeds) - 1)
+        novoAtomoAdicionado = clusters[i].vizinhos[0]
+        clusters[i].elementos.append(novoAtomoAdicionado)
+        clusters[i] = Cluster(clusters[i].elementos,
+                retornaVizinhosAtualizado(clusters[i], novoAtomoAdicionado)) 
+
+        clusters = [ Cluster(x.elementos, [ v for v in x.vizinhos 
+                if not (v is novoAtomoAdicionado) ]) for x in clusters  ]
+
+        clustersComVizinhos = [ x for x in clusters if len(x.vizinhos) > 0 ]
+
+    return clusters
+
+Ponto = namedtuple('Ponto',['x','y'])
+tamanhoXY = Ponto(25,25)
+
 """
-def divClustersSameSize(atomos, numClusters):
+    Vc tem que tratar o fato de que determinados circulos n passam por n seeds
 
-    def criaClusterVizinhos(atomoSeed, atomos, tamanhoCluster):
-        atomosNaoSelecionados = [ atomo for atomo in atomos 
-                and atomo not is atomoSeed ]
-        cluster = []
-        while len(cluster) < tamanhoCluster and len(atomosNaoSelecionados) > 0:
+    tem que tratar numero de seeds não divisivel pelo numero de circulos
+"""
+numCirculos = 2; raioCirculoMenor = 5; proporcaoCirculos = 2; numSeeds = 8; 
+atomosGulosos = geraCidadeAleatoria(tamanhoXY)
+posicoesSeeds = determinaPosicoesSeeds(
+        numSeeds, numCirculos, raioCirculoMenor, proporcaoCirculos, tamanhoXY)
 
- """       
+seedsGulosos = achaSeeds(atomosGulosos, posicoesSeeds)
+print("Desvio padrão qtdCrimes Clusterização Gulosa")
 
+print([sum([x.qtdCrimes for x in cluster.elementos]) 
+        for cluster in clusterizaGuloso(seedsGulosos)])
 
+print(pstdev([sum([x.qtdCrimes for x in cluster.elementos]) 
+        for cluster in clusterizaGuloso(seedsGulosos)]))
+
+atomosAleatorios = deepcopy(atomosGulosos)
+seedsAleatorios = achaSeeds(atomosAleatorios, posicoesSeeds)
+print("Desvio padrão CLusterização Aleatória")
+
+print([sum([x.qtdCrimes for x in cluster.elementos]) 
+        for cluster in clusterizaAleatorio(seedsAleatorios)])
+
+print(pstdev([sum([x.qtdCrimes for x in cluster.elementos]) 
+        for cluster in clusterizaAleatorio(seedsAleatorios)]))
