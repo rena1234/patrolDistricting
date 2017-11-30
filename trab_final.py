528f6b4760ab80cc25cdc11b0b8d5d86de7ec17c
Renato Ferreira <renatoferreira249@gmail.com>
2017-11-30 13:02:18 -0200

    Update trab_final.py

--- trab_final.py
+++ trab_final.py
@@ -1,15 +1,42 @@
 from collections import namedtuple
 from random import randint
 
-Cluster = namedtuple('Cluster', ['elementos', 'criminalidade'])
+Cluster = namedtuple('Cluster', ['atomos', 'criminalidade', 'num_cluster'])
 Atomo = namedtuple('Atomo', ['nome', 'posicao', 'qtdCrimes', 'vizinhos'
         ,'num_cluster'])
 Ponto = namedtuple('Ponto', ['x','y'])
 
-def retorna_rand_viz_outro_cluster(num_cluster, vizinhos)
+def retorna_atm_fronteira(cluster):
+	atms_ja_visitados = []
+	while(True):
+		atm = cluster.atomos[randint(0,len(cluster.atomos))]
+		if(len([a in atm.vizinhos if a.num_cluster != cluster.num_cluster ])):
+			return atm
 
-def verifica_compact_tax(clusters):
-    return compact_tax
+def retorna_viz_outro_cluster(atm_front):
+	atms_ja_visitados = []
+	while(True)
+		atm = atm_front_.vizinhos[randint(0,len(atm_front.vizinhos) -1)]
+		if not atm in atms_ja_visitados and atm.num_cluster != atm_front.num_cluster:
+			return atm	
+		atms_ja_visitados = atm
+
+	
+
+def retorna_compact_tax(clusters):
+	def retorna_ind_mais_violento(clusters):
+		indice = 0
+		for i,cluster in enumerate(clusters):
+			if cluster.criminalidade > clusters[indice].criminalidade:
+				indice = i;
+	def retorna_ind_menos_violento(clusters):
+		indice = 0
+		for i,cluster in enumerate(clusters):
+			if cluster.criminalidade < clusters[indice].criminalidade:
+				indice = i;
+	menor_violencia = clusters[retorna_ind_menos_violento(clusters)].criminalidade
+    maior_violencia = clusters[retorna_ind_mais_violento(clusters)].criminalidade
+    return maior_violencia / menor_violencia;
 
 def gera_sol_vizinha(clusters, compact_tax):
     cl_perdedor = clusters[randint(0,len(clusters) -1)]
@@ -21,5 +48,3 @@ def sim_annealing(clusters, n, t_inicial, t_final, a, compact_tax):
     while temp > t_final:
 
     return clusters_atualizados
-
-
